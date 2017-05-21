90fd49c6e63c24447a34d8799a0c940dc497a903
Anamnesis875 <sergey_ramzaev@mail.ru>
2017-05-21 15:16:08 +0300

    add FireBlaster script

---
+++ Assets/Scripts/FireBlaster.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This script is attached to the player and allows
+/// them to fire the Blaster projectile.
+/// </summary>
+
+public class FireBlaster : MonoBehaviour
+{
+    // Variables Start_____________________________________________________________________________
+    // The blaster projectile is attached to this in the
+    // inspector.
+    public GameObject blaster;
+
+    // Quick references.
+    private Transform myTransform;
+    private Transform cameraHeadTransform;
+
+    // The position at which the projectile should be
+    // instantiated.
+    private Vector3 launchPosition = new Vector3();
+
+    // Used to control the rate of fire.
+    private float fireRate = 0.2f;
+    private float nextFire = 0;
+    // Variables End_______________________________________________________________________________
+
+    // Use this for initialization
+    void Start ()
+    {
+        myTransform = transform;
+        cameraHeadTransform = myTransform.FindChild("CameraHead");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetButton("Fire Weapon") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            // The launch position of the projectile will be just in front of
+            // the cameraHead
+            launchPosition = cameraHeadTransform.TransformPoint(0, 0, 0.2f);
+
+            // Create the blaster projectile at the launchPosition and tilt its angle
+            // so that its horizontal using the angle eulerAngles.x + 90.
+            Instantiate(blaster, launchPosition, Quaternion.Euler(cameraHeadTransform.eulerAngles.x + 90,
+                                                                  cameraHeadTransform.eulerAngles.y, 0));
+        }
+	}
+}
